523122c352dde32479f4c337990d8b973ddc2c0f
Lilla <lillaev@yahoo.com>
2021-08-24 15:41:12 +0200

    html from js

--- calc.js
+++ calc.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const calculator = document.querySelector('.calculator');
+
 const Calculator = {
   prevText: null,
   currentText: null,
@@ -9,22 +11,36 @@ const Calculator = {
     this.prevText = document.querySelector('.prev');
     this.currentText = document.querySelector('.current');
 
-    document.querySelectorAll('.btn').forEach(btn =>
-        btn.addEventListener('click', e => this.clickHandler(e))
-    );
+    document
+      .querySelectorAll('.btn')
+      .forEach((btn) =>
+        btn.addEventListener('click', (e) => this.clickHandler(e))
+      );
   },
 
-  clickHandler: function(e) {
+  clickHandler: function (e) {
     const buttonText = e.currentTarget.innerText;
 
     switch (e.currentTarget.innerText) {
-      case 'RES': this.clearHandler(); break;
-      case 'DEL': this.deleteHandler(); break;
+      case 'RES':
+        this.clearHandler();
+        break;
+      case 'DEL':
+        this.deleteHandler();
+        break;
       case '+':
-      case '-': this.chooseOperation(buttonText); break;
-      case '=': this.calculate(); break;
-      case '- / +': this.toggleNegativeHandler(); break;
-      default: this.appendNumber(buttonText); break;
+      case '-':
+        this.chooseOperation(buttonText);
+        break;
+      case '=':
+        this.calculate();
+        break;
+      case '- / +':
+        this.toggleNegativeHandler();
+        break;
+      default:
+        this.appendNumber(buttonText);
+        break;
     }
   },
 
@@ -34,7 +50,9 @@ const Calculator = {
   },
 
   deleteHandler: function () {
-    this.currentText.innerText = this.currentText.innerText.toString().slice(0, -1);
+    this.currentText.innerText = this.currentText.innerText
+      .toString()
+      .slice(0, -1);
     if (this.currentText.innerText === '') {
       this.currentText.innerText = this.prevText.innerText;
       this.prevText.innerText = '';
@@ -42,7 +60,8 @@ const Calculator = {
   },
 
   appendNumber: function (buttonText) {
-    this.currentText.innerText = this.currentText.innerText.toString() + buttonText.toString();
+    this.currentText.innerText =
+      this.currentText.innerText.toString() + buttonText.toString();
   },
 
   toggleNegativeHandler: function () {
@@ -69,13 +88,42 @@ const Calculator = {
 
     if (isNaN(previousValue) || isNaN(currentValue)) return;
 
-    result = buttonText === '+'
+    result =
+      buttonText === '+'
         ? previousValue + currentValue
         : previousValue - currentValue;
 
     this.currentText.innerText = result;
     this.prevText.innerText = '';
-  }
+  },
 };
+
+const renderCalculator = () => {
+  let output = `
+  <div class="calculation">
+  <div class="prev"></div>
+  <div class="current"></div>
+</div>
+<button class="bigger reset btn">RES</button>
+<button class="bigger delete btn">DEL</button>
+<button class="nr btn">1</button>
+<button class="nr btn">2</button>
+<button class="nr btn">3</button>
+<button class="plus operator btn">+</button>
+<button class="nr btn">4</button>
+<button class="nr btn">5</button>
+<button class="nr btn">6</button>
+<button class="minus operator btn">-</button>
+<button class="nr btn">7</button>
+<button class="nr btn">8</button>
+<button class="nr btn">9</button>
+<button class="btn toggle">- / +</button>
+<button class="nr btn">0</button>
+<button class="bigger equal btn">=</button>
+  `;
+  calculator.insertAdjacentHTML('afterbegin', output);
+};
+
+renderCalculator();
 
 Calculator.init();
